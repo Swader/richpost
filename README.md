50d49b27403f213db8f981fc02810fc5bb7354f7
Bruno Skvorc <bruno@skvorc.me>
2013-09-20 11:41:22 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# Richpost
+
+Richpost is a Chrome Extension which enables Markdown support in Google Plus.
+
+For more information on how and why it was built, see [the blog post](http://www.bitfalls.com/2013/09/markdown-support-in-google-plus-chrome.html).
+
+## License
+
+See [license](LICENSE.md).
+
+## Contributions
+
+Accepting pull requests. Go crazy.
+
+### Todo:
+
+ - support for embedding gists. The idea is to detect a gist URL in a #richpost marked post, and offer a "load" option next to it, which then loads the gist in place. This is so that the whole page doesn't screech to a halt by trying to load a bunch of gist laden posts. An option in the options page might be used to turn auto-loading of gists on and off.
+ - support for Facebook? (it might even actually already work, seeing as the code is quite platform agnostic)
+ - a switch to "unMD" the post, as in "show/hide parsed"
+ - a way to ignore hashtags. I currently hide their contents to prevent weird headings.
+ - performance comparisons between XPath (currently used for the most part) and native document methods for finding/filtering elements.
+ - fix highlighting, it selects oddly arbitrary languages right now.
+ - option to choose highlight theme (all are packed in the extension by default, just needs a switch of some kind)
+ - stop intervals when tab is inactive
---
+++ source/content.js
@@ -0,0 +1,141 @@
+/**
+ * Object to contain hashes of processed posts, and their boolean flags on whether or not they've been processed
+ * @type {{}}
+ */
+var processedPosts = {};
+
+/**
+ * A variable that holds the parent element in which posts are rendered. Shrinks XPATH context, increasing performance.
+ * @type {null}
+ */
+var postsContainer = null;
+
+/**
+ * Shorthand for the XPATH fetch constant we use regularly
+ * @type {Number}
+ */
+const xpo = XPathResult.ORDERED_NODE_SNAPSHOT_TYPE;
+
+/**
+ * Init marked with default options
+ */
+marked.setOptions({
+    gfm: true,
+    langPrefix: 'language-'
+});
+
+/**
+ * Init flags for added listeners to sharebox controls. This will serve to refresh the MD after an edit or post.
+ * @type {{Save: boolean, Cancel: boolean}}
+ */
+var listenersAdded = {
+    Save: false,
+    Cancel: false
+};
+
+/**
+ * Re-runs the MD over all posts on edit or cancel, to make sure the posts are all rendered up to date
+ * It also resets event listeners to avoid duplicates
+ * @param e
+ */
+function resetProcessed(e) {
+    processedPosts = {};
+    listenersAdded.Save = false;
+    listenersAdded.Cancel = false;
+    e.target.removeEventListener('click', resetProcessed);
+}
+
+/**
+ * Fetches the Share/Save and Cancel buttons from the Sharebox and binds resetProcessed() to their click
+ */
+function restoreListeners() {
+    var scope = document.evaluate('//div[@guidedhelpid="shareboxcontrols"]', document, null, xpo).snapshotItem(0);
+    if (scope !== null && scope !== undefined && processedPosts != {}) {
+        var buttons = document.evaluate('//div[@role="button"]', scope, null, xpo);
+        if (buttons.snapshotItem(0) && !listenersAdded.Save) {
+            buttons.snapshotItem(0).addEventListener('click', resetProcessed);
+            listenersAdded.Save = true;
+        }
+        if (buttons.snapshotItem(1) && !listenersAdded.Cancel) {
+            buttons.snapshotItem(1).addEventListener('click', resetProcessed);
+            listenersAdded.Cancel = true;
+        }
+    }
+}
+restoreListeners();
+
+/**
+ * Check for MD-able posts every 2 seconds
+ */
+chrome.extension.sendMessage({}, function (response) {
+    var readyStateCheckInterval = setInterval(function () {
+        if (document.readyState === "complete") {
+            clearInterval(readyStateCheckInterval);
+            convertTexts(findHashTags());
+            setInterval(function () {
+                convertTexts(findHashTags());
+            }, 2000);
+        }
+    }, 10);
+});
+
+/**
+ * Find all #richpost hashtags to find MD-able posts
+ * @returns {Object}
+ */
+function findHashTags() {
+    if (postsContainer === undefined || postsContainer === null) {
+        postsContainer = document.getElementsByClassName('ow')[0];
+    }
+    var evalString = '//a[@class="ot-hashtag" and text()="#richpost"]';
+    return document.evaluate(evalString, postsContainer, null, xpo);
+}
+
+/**
+ * Convert all MD-able posts to MD content.
+ * @param xpathresult
+ */
+function convertTexts(xpathresult) {
+    for (var i = 0; i < xpathresult.snapshotLength; i++) {
+        var parentElement = xpathresult.snapshotItem(i).parentNode;
+        if (parentElement !== null && parentElement !== undefined) {
+            var hashCode = parentElement.innerHTML.hashCode();
+            if (processedPosts[hashCode] === undefined) {
+                processedPosts[hashCode] = false;
+            }
+            if (processedPosts[hashCode] === false) {
+                // Find all hashtags in this element and empty them. @todo fix this to ignore hashtags, not empty
+                var hashes = document.evaluate('//a[@class="ot-hashtag"]', parentElement, null, xpo);
+                for (var j = 0; j < hashes.snapshotLength; j++) {
+                    hashes.snapshotItem(j).innerText = "";
+                }
+
+                parentElement.innerHTML = marked(parentElement.innerText);
+
+                var codeBlocks = document.evaluate('//pre[code]', parentElement, null, xpo);
+                for (var h = 0; h < codeBlocks.snapshotLength; h++) {
+                    hljs.highlightBlock(codeBlocks.snapshotItem(h));
+                }
+
+                processedPosts[hashCode] = true;
+
+            }
+        }
+    }
+}
+
+/**
+ * A helper method to get the hashCode of a post
+ * @returns {number}
+ */
+String.prototype.hashCode = function () {
+    var hash = 0;
+    if (this.length == 0) return hash;
+    var l = this.length;
+    for (i = 0; i < l; i++) {
+        var c = this.charCodeAt(i);
+        hash = ((hash << 5) - hash) + c;
+        hash = hash & hash; // Convert to 32bit integer
+    }
+    return hash;
+}
